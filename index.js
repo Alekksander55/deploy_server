4dcc1123069b1fdb2ba9b1fae9854614a0d80839
Alekksander55 <alexandertemim@gmail.com>
2024-02-04 09:58:54 +0200

    add env file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express();
 app.use(cors());
 
 app.get("/api/:name", (req, res) => {
-  res.json({ message: `Hello ${req.params.name}, from server!` });
+  res.json({ message: `Hello ${req.params.name}, from server! ${process.env.NY_NAME}` });
 });
 
 app.listen(process.env.PORT || 3001, () => {
